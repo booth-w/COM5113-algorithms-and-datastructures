d99de9168abe428ef4e9de4b2f5a43f9bb278345
booth-w <personal@booth-w.co.uk>
2025-10-16 16:37:27 +0100

    Add more tests

--- test.cs
+++ test.cs
@@ -12,9 +12,8 @@ class Test {
 		list.PushFront(1);
 		list.PushFront(2);
 		list.PushFront(3);
-
 		string output = list.PrintList();
-		Debug.Assert(output == "3, 2, 1", $"push front found: {output}, expected: 3, 2, 1");
+		Debug.Assert(output == "3, 2, 1", $"push front. found: {output}, expected: 3, 2, 1");
 	}
 
 	public void PopFront() {
@@ -26,22 +25,31 @@ class Test {
 		list.PopFront();
 
 		string output = list.PrintList();
-		Debug.Assert(output == "2, 1", $"pop front found: {output}, expected: 2, 1");
+		Debug.Assert(output == "2, 1", $"pop front. found: {output}, expected: 2, 1");
+
+		list.PopFront();
+		list.PopFront();
+		output = list.PrintList();
+		Debug.Assert(output == "", $"pop front last. found: {output}, expected: empty string");
+
+		list.PopFront();
+		output = list.PrintList();
+		Debug.Assert(output == "", $"pop front empty. found: {output}, expected: empty string");
 	}
 
 	public void Count() {
 		LinkedList list = new LinkedList();
 
 		int count = list.Count();
-		Debug.Assert(count == 0, $"count found: {count}, expected: 0");
+		Debug.Assert(count == 0, $"count 0. found: {count}, expected: 0");
 
 		list.PushFront(1);
 		count = list.Count();
-		Debug.Assert(count == 1, $"count found: {count}, expected: 1");
+		Debug.Assert(count == 1, $"count 1. found: {count}, expected: 1");
 
 		list.PushFront(2);
 		list.PushFront(3);
 		count = list.Count();
-		Debug.Assert(count == 3, $"count found: {count}, expected: 3");
+		Debug.Assert(count == 3, $"count 3. found: {count}, expected: 3");
 	}
 }
