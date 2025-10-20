936d0f8452da38b8e85e0f5d085672cdcd38092f
booth-w <personal@booth-w.co.uk>
2025-10-20 12:30:59 +0100

    Add linked list

---
+++ linked-list.cs
@@ -0,0 +1,124 @@
+using System;
+
+public class LinkedList<T> {// where T : IComparable<T> {
+	private Element<T>? _head;
+
+	public LinkedList() {
+		_head = null;
+	}
+
+	public int Count() {
+		int count = 0;
+		Element<T>? current = _head;
+
+		while (current != null) {
+			count++;
+			current = current.Next;
+		}
+
+		return count;
+	}
+
+	public void PushFront(T data) {
+		Element<T> newElement = new Element<T>(data);
+		newElement.Next = _head;
+		_head = newElement;
+	}
+
+	public void PopFront() {
+		Element<T>? oldHead = _head;
+		if (oldHead != null) {
+			_head = oldHead.Next;
+		}
+	}
+
+	// public void PushSorted(T data) {
+	// 	Element<T> newElement = new Element<T>(data);
+	//
+	// 	if (_head == null || _head.Data.CompareTo(data) >= 0) {
+	// 		newElement.Next = _head;
+	// 		_head = newElement;
+	// 		return;
+	// 	}
+	//
+	// 	Element<T>? current = _head;
+	// 	while (current.Next != null && current.Next.Data.CompareTo(data) < 0) {
+	// 		current = current.Next;
+	// 	}
+	// 	newElement.Next = current.Next;
+	// 	current.Next = newElement;
+	// }
+
+	public bool GetFront(ref T data) {
+		bool listNotEmpty = _head != null;
+		if (listNotEmpty) {
+			data = _head.Data;
+		} else {
+			Console.WriteLine("Cannot get front of an empty list");
+		}
+
+		return listNotEmpty;
+	}
+
+	public void RemoveFirst(T data) {
+		if (_head == null) {
+			return;
+		}
+
+		if (_head.Data.Equals(data)) {
+			_head = _head.Next;
+			return;
+		}
+
+		Element<T>? current = _head;
+		while (current.Next != null) {
+			if (current.Next.Data.Equals(data)) {
+				current.Next = current.Next.Next;
+				return;
+			}
+			current = current.Next;
+		}
+	}
+
+	public bool Contains(T data) {
+		Element<T>? current = _head;
+
+		while (current != null) {
+			if (current.Data.Equals(data)) {
+				return true;
+			}
+			current = current.Next;
+		}
+
+		return false;
+	}
+
+	public void Copy(Element<T>? newList) {
+		// TODO
+		if (_head == null) {
+			return;
+		}
+
+		Element<T>? current = _head;
+		while (current != null) {
+
+		}
+	}
+
+	public string PrintList() {
+		Element<T>? current = _head;
+
+		string toPrint = "";
+		while (current != null) {
+			toPrint += current.Data + ", ";
+			current = current.Next;
+		}
+
+		// remove last ", "
+		if (toPrint.Length >= 2) {
+			toPrint = toPrint[..^2];
+		}
+
+		return toPrint;
+	}
+}
