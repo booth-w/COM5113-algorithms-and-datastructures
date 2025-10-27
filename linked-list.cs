32626213cc9b2402b93057aa498af64310e3bf43
booth-w <personal@booth-w.co.uk>
2025-10-27 11:50:10 +0000

    Add task 1 unknown methods

--- linked-list.cs
+++ linked-list.cs
@@ -121,4 +121,39 @@ public class LinkedList<T> {
 
 		return toPrint;
 	}
+
+	private Element RoutineA(int val, Element list) {
+		if (list == null) {
+			return new Element(val);
+		} else if (list.Data >= val) {
+			Element newElem = new Element(val);
+			newElem.Next = list;
+			return newElem;
+		} else {
+			list.Next = RoutineA(val, list.Next);
+			return list;
+		}
+	}
+
+	private Element RoutineB(int val, Element list) {
+		if (list == null) {
+			return null;
+		} else if (list.Data == val) {
+			return list.Next;
+		} else {
+			List.Next = RoutineB(val, list.Next);
+			return list;
+		}
+	}
+
+	private Element RoutineC(int val, Element list) {
+		if (list == null) {
+			return null;
+		} else if (list.Data == val) {
+			return RoutineC(val, list.Next);
+		} else {
+			List.Next = RoutineC(val, list.Next);
+			return list;
+		}
+	}
 }
